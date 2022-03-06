cc38880fa47d01b96b1a1c2704471c18b0072a10
Cormorant Programming <cormorant.programming@gmail.com>
2022-03-06 17:24:31 +0700

    formatting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+
+function App () {
+
   return (
+
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <img src={ logo } className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,9 +24,14 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
+
     </div>
-  );
+
+  )
+
 }
 
-export default App;
+
+export default App
