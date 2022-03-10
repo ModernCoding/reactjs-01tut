1a9296d2cdb7b5288ee45a6e74eb337bdb31c2de
Cormorant Programming <cormorant.programming@gmail.com>
2022-03-10 10:49:28 +0700

    list & array

--- src/Content.js
+++ src/Content.js
@@ -49,32 +49,42 @@ const Content = () => {
 
     <main>
 
-      <ul>
-        {
-          items.map (item => (
-            
-            <li className="item" key={ item.id }>
-              <input
-                id={`item-${ item.id }`}
-                type="checkbox"
-                checked={ item.checked }
-                onChange={ () => handleCheck (item.id) }
-              />
-              
-              <label htmlFor={`item-${ item.id }`}>
-                { item.item }
-              </label>
+      { items.length ? (
 
-              <FaTrashAlt
-                role="button"
-                tabIndex="0"
-                onClick={ () => handleDelete (item.id) }
-              />
-            </li>  
-          
-          ))
-        }
-      </ul>
+          <ul>
+            {
+              items.map (item => (
+                
+                <li className="item" key={ item.id }>
+                  <input
+                    id={`item-${ item.id }`}
+                    type="checkbox"
+                    checked={ item.checked }
+                    onChange={ () => handleCheck (item.id) }
+                  />
+                  
+                  <label htmlFor={`item-${ item.id }`}>
+                    { item.item }
+                  </label>
+
+                  <FaTrashAlt
+                    role="button"
+                    tabIndex="0"
+                    onClick={ () => handleDelete (item.id) }
+                  />
+                </li>  
+              
+              ))
+            }
+          </ul>
+
+        ) : (
+
+          <strong>NO ITEM!</strong>
+
+        ) 
+        
+      }
       
     </main>
 
